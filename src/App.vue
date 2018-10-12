dce746fffd69992d1412397914991b4fee70897a
liugy <lgy_hk@163.com>
2018-10-12 11:23:12 +0800

    code updated

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <vPagination :total="total" :current-page='current' :pagegroup="pagegroup" @pagechange="pagechange" v-if="isShowPagination"></vPagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      total: 1,
+      current: 1,
+      pagegroup: 10,
+      isShowPagination: true
+    }
+  },
+  methods: {
+    pagechange (currentPage) {
+      console.log('currentPage=', currentPage)
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
